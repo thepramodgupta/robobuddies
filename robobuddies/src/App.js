1d0974af245e76fbb70f63ac7e581de3e7e3467b
Pramod Gupta <P10484337@capita.co.uk>
2021-02-14 19:33:57 +0530

    class and used state

--- robobuddies/src/App.js
+++ robobuddies/src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './Searchbox';
 import { robots } from './robots';
 
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+        robots: robots,
+        searchfield: ''
+        }
+    }
 
-const App = () => {
-    return (
-        <div className='tc'>
-            <SearchBox/>
-            <h1>Robo Friends</h1>
-            <CardList robots={robots}/>
-        </div>          
-    );
-};
+    onSearchChange = (event) => {
+        this.setState(({searchfield:event.target.value}));
+    }
 
+    render () {
+        const filteredRobots = this.state.robots.filter( robot => {
+        return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
+
+        return (
+            <div className='tc'>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <h1>Robo Friends</h1>
+                <CardList robots={filteredRobots}/>
+            </div>          
+        );
+    }
+    
+}
 export default App;
